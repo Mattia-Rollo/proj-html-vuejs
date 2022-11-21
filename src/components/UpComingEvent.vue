44eb8b32722aa34f21a996b83ab3132ebd7f42fb
mattia.rollo <ste.tia@hotmail.it>
2022-11-22 00:53:19 +0100

    add cards in UpComingEvent

--- src/components/UpComingEvent.vue
+++ src/components/UpComingEvent.vue
@@ -9,15 +9,22 @@
                     </div>
                     <h2 class="text-center pb-5">Lest's Work Together</h2>
                 </div>
-                <div class="row gx-5 ">
-                    <div class="col">
-                        <div class="mycard">
-
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="mycard">
-
+                <div class="row g-3">
+                    <div class="col-6" v-for="(item, index) in listEvents" :key="index">
+                        <div class="mycard d-flex">
+                            <div v-if="index < 2">
+                                <img :src="`/images/artist-event-${item.path_img}-250x300.jpg`" alt="" class="h-100">
+                            </div>
+                            <div class="p-2 d-flex flex-column justify-content-center"
+                                :class="{ 'text-end': index >= 2 }">
+                                <div class="text-secondary">{{ item.date }}</div>
+                                <h4>{{ item.event }}</h4>
+                                <div class="text-secondary"><i class="fa-solid fa-location-dot"></i>{{ item.location }}
+                                </div>
+                            </div>
+                            <div v-if="index >= 2">
+                                <img :src="`/images/artist-event-${item.path_img}-250x300.jpg`" alt="" class="h-100">
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -28,10 +35,42 @@
 
 <script>
 export default {
-    name: 'UpComingEvent'
+    name: 'UpComingEvent',
+    data() {
+        return {
+            listEvents: [
+                {
+                    path_img: '04',
+                    date: 'NOV 22, 2020',
+                    event: 'Storytelling Workshop',
+                    location: 'Texas, US'
+                },
+                {
+                    path_img: '03',
+                    date: 'OCT 10, 2020',
+                    event: 'Painting Art Contest 2020',
+                    location: 'New York, US'
+                },
+                {
+                    path_img: '02',
+                    date: 'NOV 23, 2020',
+                    event: 'International Art Fair 2020',
+                    location: 'Hamburg, Germany'
+                },
+                {
+                    path_img: '01',
+                    date: 'DEC 15, 2020',
+                    event: 'Street Performance: Call for Artist',
+                    location: 'Illinois, US'
+                },
+            ]
+        }
+    },
 }
 </script>
 
 <style lang="scss" scoped>
-
+.mycard {
+    height: 200px;
+}
 </style>
