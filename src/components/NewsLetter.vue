4952e3c801910bc256b3d4a054920bc0538911c5
mattia.rollo <ste.tia@hotmail.it>
2022-11-22 09:56:41 +0100

    style newsletter

--- src/components/NewsLetter.vue
+++ src/components/NewsLetter.vue
@@ -1,23 +1,23 @@
 <template>
-    <div class="wrapper">
+    <div class="wrapper d-flex">
         <img src="/images/artist-shape-color-paint-top-left.png" alt="">
-        <img src="/images/artist-shape-color-paint-bottom-right.png" alt="">
-        ciao sono newsletter
-        <div class="mycontainer h-100 px-5">
-            <div class="container-lg h-100">
-                <div class="row align-items-center text-light">
-                    <div class="col ">
-                        <h3 class=" text-end w-75">NewsLetter to get in Touch</h3>
-                    </div>
-                    <div class="col-8 ">
-                        <a href="">
-                            <div id="email" class="border-bottom border-1 fs-4 d-flex justify-content-between">Your
-                                Email <i class="fa-solid fa-arrow-right"></i></div>
-                        </a>
-                    </div>
+
+
+        <div class="container-lg ">
+            <div class="row align-items-center text-light h-100">
+                <div class="col ">
+                    <h3 class=" text-end w-75">NewsLetter to get in Touch</h3>
+                </div>
+                <div class="col-8 ">
+                    <a href="">
+                        <div id="email" class="border-bottom border-1 fs-4 d-flex justify-content-between">Your
+                            Email <i class="fa-solid fa-arrow-right"></i></div>
+                    </a>
                 </div>
             </div>
         </div>
+
+        <img src="/images/artist-shape-color-paint-bottom-right.png" alt="">
     </div>
 </template>
 
@@ -37,17 +37,17 @@ export default {
     overflow: hidden;
 
     img {
-        position: absolute;
+        position: relative;
+        height: 100%;
     }
 
     img:first-child {
         left: 0;
-        height: 100%;
     }
 
     img:nth-child(2) {
         right: 0;
-        height: 100%;
+
     }
 
     a {
