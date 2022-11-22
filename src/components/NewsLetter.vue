0989366d2c7cb09835fbce1d86bfb5f68b1fbf8c
mattia.rollo <ste.tia@hotmail.it>
2022-11-22 01:20:02 +0100

    style newsletter

--- src/components/NewsLetter.vue
+++ src/components/NewsLetter.vue
@@ -3,6 +3,21 @@
         <img src="/images/artist-shape-color-paint-top-left.png" alt="">
         <img src="/images/artist-shape-color-paint-bottom-right.png" alt="">
         ciao sono newsletter
+        <div class="mycontainer h-100 px-5">
+            <div class="container-lg h-100">
+                <div class="row align-items-center text-light">
+                    <div class="col ">
+                        <h3 class=" text-end w-75">NewsLetter to get in Touch</h3>
+                    </div>
+                    <div class="col-8 ">
+                        <a href="">
+                            <div id="email" class="border-bottom border-1 fs-4 d-flex justify-content-between">Your
+                                Email <i class="fa-solid fa-arrow-right"></i></div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -16,7 +31,7 @@ export default {
 @use '../assets/style/partials/variables' as *;
 
 .wrapper {
-    height: 100px;
+    height: 150px;
     background-color: $blue-bg-newsletter;
     position: relative;
     overflow: hidden;
@@ -33,6 +48,11 @@ export default {
     img:nth-child(2) {
         right: 0;
         height: 100%;
+    }
+
+    a {
+        text-decoration: none;
+        color: $white;
     }
 }
 </style>
