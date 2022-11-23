f7178f249751ef8edc699cee49851f75c6d076aa
mattia.rollo <ste.tia@hotmail.it>
2022-11-23 11:58:49 +0100

    try mousemove event

--- src/components/JumbotronComp.vue
+++ src/components/JumbotronComp.vue
@@ -1,27 +1,27 @@
 <template>
-    <div class="jumbotron">
-        <img src="../../public/images/artist-shape-01.png" alt="" class="w-50">
-        <img src="../../public/images/artist-shape-02-768x415.png" alt="" class="w-50">
-        <img src="../../public/images/artist-shape-03.png" alt="" class="puntini">
-        <img src="../../public/images/artist-shape-04-127x300.png" alt="" class="w-10 piuma">
-        <img src="../../public/images/artist-shape-05-300x198.png" alt="" class="">
-        <div class="face"><img src="../../public/images/artist-hero-image-03.png"></div>
+    <div class="jumbotron" @mousemove="mouseMove">
+        <img src="/images/artist-shape-01.png" alt="" class="w-50">
+        <img src="/images/artist-shape-02-768x415.png" alt="" class="w-50">
+        <img src="/images/artist-shape-03.png" alt="" class="puntini">
+        <img src="/images/artist-shape-04-127x300.png" alt="" class="w-10 piuma">
+        <img id="macchia-pennello" src="/images/artist-shape-05-300x198.png" alt="" class="">
+        <div class="face"><img src="/images/artist-hero-image-03.png"></div>
         <div class="pittura">
-            <img src="../../public/images/artist-hero-image-02.jpg" alt="" class="img-fluid">
+            <img src="/images/artist-hero-image-02.jpg" alt="" class="img-fluid">
         </div>
-        <div class="artist">
-            <img src="../../public/images/artist-hero-image-04.jpg" alt="" class="img-fluid">
+        <div class="artist" :style="{ right: artist.x ? artist.x : '' + 'px', top: artist.y + 'px' }">
+            <img src="/images/artist-hero-image-04.jpg" alt="" class="img-fluid">
         </div>
         <div class="monkey">
-            <img src="../../public/images/artist-hero-image-01.jpg" alt="" class="img-fluid">
+            <img src="/images/artist-hero-image-01.jpg" alt="" class="">
         </div>
-        <svg id="macchia-azzurra" xmlns="http://www.w3.org/2000/svg" version="1.1" width="350px" height="350px"
+        <svg id="macchia-azzurra" xmlns="http://www.w3.org/2000/svg" version="1.1" width="25vw" height="25vw"
             viewBox="0 0 200 202">
             <path class="fill" fill="#b1e4f4"
                 d="M139.374493,198.087515 C155.702187,206.99353 190.724846,197.001323 197.677982,159.049528 C204.827153,120.027728 184.496324,118.997566 188.045232,104.801934 C202.287755,83.6528313 204.827153,56.662651 190.073179,40.4146034 C153.570125,2.39058926 144.444362,48.0194062 114.025151,20.1351292 C101.727304,7.83728238 83.0989526,-6.73517414 56.2286492,3.40456297 C18.2385839,17.7404371 27.3303985,47.659333 27.3303985,69.3128542 C27.3303985,76.9370868 3.11408002,91.2696589 0.460095291,110.885776 C-2.88628833,135.619549 12.6277797,168.175291 27.3303985,179.329002 C50.6517938,205.692318 86.6478605,199.608476 102.87144,190.482713 C116.802125,182.646702 128.220783,192.003673 139.374493,198.087515 Z">
             </path>
         </svg>
-        <svg id="macchia-gialla" xmlns="http://www.w3.org/2000/svg" version="1.1" width="200px" height="200px"
+        <svg id="macchia-gialla" xmlns="http://www.w3.org/2000/svg" version="1.1" width="15vw" height="15vw"
             viewBox="0 0 200 202">
             <path class="fill" fill="#efab43"
                 d="M139.374493,198.087515 C155.702187,206.99353 190.724846,197.001323 197.677982,159.049528 C204.827153,120.027728 184.496324,118.997566 188.045232,104.801934 C202.287755,83.6528313 204.827153,56.662651 190.073179,40.4146034 C153.570125,2.39058926 144.444362,48.0194062 114.025151,20.1351292 C101.727304,7.83728238 83.0989526,-6.73517414 56.2286492,3.40456297 C18.2385839,17.7404371 27.3303985,47.659333 27.3303985,69.3128542 C27.3303985,76.9370868 3.11408002,91.2696589 0.460095291,110.885776 C-2.88628833,135.619549 12.6277797,168.175291 27.3303985,179.329002 C50.6517938,205.692318 86.6478605,199.608476 102.87144,190.482713 C116.802125,182.646702 128.220783,192.003673 139.374493,198.087515 Z">
@@ -41,9 +41,24 @@ export default {
     name: 'JumbotronComp',
     data() {
         return {
-
+            monkey: {
+                x: 12,
+                y: 100
+            },
+            artist: {
+                x: 10,
+                y: 100
+            }
         }
     },
+    methods: {
+
+        mouseMove(event) {
+            this.artist.x = event.clientX / 15 * 0.5
+            this.artist.y = (event.clientY + 200) / 10
+            console.log(event.clientY, this.artist.y);
+        }
+    }
 }
 </script>
 
@@ -57,7 +72,7 @@ export default {
     overflow: hidden;
 
     img {
-        max-width: 100%;
+        // max-width: 100%;
     }
 
     &>* {
@@ -70,22 +85,26 @@ export default {
         z-index: 30;
     }
 
-    img[src*="-shape-02"] {
+    .macchia-pennello {
         // width: 200px;
         z-index: 10;
     }
 
     .monkey {
-        width: 12vw;
+        width: 10vw;
         bottom: 10%;
-        left: 5%;
+        left: 5vw;
         z-index: 200;
+
+        img {
+            width: 15vw;
+        }
     }
 
     .artist {
-        width: 15vw;
-        right: 8%;
-        top: 30%;
+        width: 20vw;
+        right: 8vw;
+        bottom: 10vh;
         z-index: 200;
     }
 
@@ -95,19 +114,26 @@ export default {
         bottom: 0;
         left: 30%;
 
+        img {
+            max-width: 100%;
+        }
+
     }
 
     .pittura {
         top: 10%;
         left: 15%;
-        width: 12vw;
+        width: 15vw;
         z-index: 70;
     }
 
     .puntini {
         left: 30px;
         bottom: 0;
-        max-width: 100%;
+        // max-width: 80%;
+        // min-width: 20vh;
+        height: 100%;
+        // height: 10vh;
         z-index: 20;
     }
 
@@ -128,8 +154,8 @@ export default {
     }
 
     svg#macchia-gialla {
-        bottom: 25px;
-        right: 185px;
+        bottom: 8vh;
+        right: 10vw;
         z-index: 150;
 
     }
